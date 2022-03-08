f8fd66413d4a75013c9cb4559e4da09c7c713eac
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2022-03-08 13:05:46 +0500

    Commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
-// Play & Pause
+// Play & Pause........................................................................................ //
 function showPlayIcon() {
   playBtn.classList.replace('fa-pause', 'fa-play');
   playBtn.setAttribute('title', 'Play');
@@ -29,14 +29,31 @@ function togglePlay() {
 // On video end, show play button icon
 video.addEventListener('ended', showPlayIcon);
 
-// Progress Bar
+// Progress Bar....................................................................................... //
 
-// Volume Controls
+// Calculate display time format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
 
-// Change Playback Speed
+// Update progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
 
-// Full Screen
+// Volume Controls....................................................................................... //
 
-// Event Listeners
+// Change Playback Speed....................................................................................... //
+
+// Full Screen....................................................................................... //
+
+// Event Listeners....................................................................................... //
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
